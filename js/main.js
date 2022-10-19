4ade01648aad96cc8d32dbb093e0fa4bb979b16c
chikamasuda <c.m.062820@gmail.com>
2022-10-19 23:56:41 +0900

    タイピングゲームを作成

---
+++ js/main.js
@@ -0,0 +1,37 @@
+'use strict'
+
+{
+  function setWord() {
+    word = words[Math.floor(Math.random() * words.length)];
+    target.textContent = word;
+    loc = 0;
+  }
+
+  const words = [
+    'red',
+    'blue',
+    'pink'
+  ];
+  let word;
+  let loc = 0;
+
+  const target = document.getElementById('target');
+
+  setWord();
+
+  document.addEventListener('keydown', e => {
+    if (e.key !== word[loc]) {
+      return;
+    }
+    loc++;
+
+    //1: _ed
+    //2: __d
+    //3: ___
+    target.textContent = '_'.repeat(loc) + word.substring(loc);
+
+    if (loc === word.length) {
+      setWord();
+    }
+  });
+}
